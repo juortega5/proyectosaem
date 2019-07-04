6a84868565f59ac0f055e0ded3b9d89c60b30b32
juortega5 <juortega5@poligran.edu.co>
2019-07-04 15:49:04 -0500

    pruebas unit

---
+++ tests/ConexionTest.php
@@ -0,0 +1,19 @@
+<?php
+    namespace tests;
+    use core\Conexion;
+    use PDO;
+    class ConexionTest extends \PHPUnit_Framework_TestCase
+    {
+        public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame(1, count($stack));
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
+    }
+    }
